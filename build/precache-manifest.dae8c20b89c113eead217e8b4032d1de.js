88062f958db2cedd82373745c20628aae55404ae
NB <47492535+altmshfkgudtjr@users.noreply.github.com>
2020-11-26 23:45:35 +0900

    build folder added

---
+++ build/precache-manifest.dae8c20b89c113eead217e8b4032d1de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da46d737c1a5da229348c7b76f8d94f",
+    "url": "/altmshfkgudtjr/ELEposition/index.html"
+  },
+  {
+    "revision": "81dd0642d2e5b4876c58",
+    "url": "/altmshfkgudtjr/ELEposition/static/js/2.9cbc3a2d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/altmshfkgudtjr/ELEposition/static/js/2.9cbc3a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b447360f4637a51aaf98",
+    "url": "/altmshfkgudtjr/ELEposition/static/js/main.0b4675b7.chunk.js"
+  },
+  {
+    "revision": "492e8cefbf255865fec8",
+    "url": "/altmshfkgudtjr/ELEposition/static/js/runtime-main.058c8d55.js"
+  }
+]);
